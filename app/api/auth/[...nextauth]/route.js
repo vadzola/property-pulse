3b4c9ad1473fe75dcc1c1b4069edfec4449e6f9b
vadzola <vadzola@yahoo.com.hk>
2024-05-23 18:16:06 +0200

    finale23

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import { authOptions } from '@/utils/authOptions';
 import NextAuth from 'next-auth/next';
 
+export const dynamic = 'force-dynamic';
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 import FeaturedProperties from '@/components/FeaturedProperties';
 
+export const dynamic = 'force-dynamic';
 const HomePage = () => {
   return (
     <>
